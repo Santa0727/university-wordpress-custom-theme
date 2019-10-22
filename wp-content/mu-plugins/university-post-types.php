da24e425831a7a7a20534d6bdc0381cd01202ccf
vsanta727 <vsanta727@outlook.com>
2019-10-21 22:54:48 -0500

    8.2 Display Custom Post Types

--- wp-content/mu-plugins/university-post-types.php
+++ wp-content/mu-plugins/university-post-types.php
@@ -2,15 +2,17 @@
 
 function university_post_types() {
   register_post_type('event', array(
-      'public' => true,
-      'labels' => array(
-          'name' => 'Events',
-          'add_new_item' => 'Add New Event',
-          'edit_item' => 'Edit Event',
-          'all_items' => 'All Events',
-          'singular_name' => 'Event'
-      ),
-      'menu_icon' => 'dashicons-calendar'
+    'rewrite' => array('slug' => 'events'),
+    'has_archive' => true,
+    'public' => true,
+    'labels' => array(
+      'name' => 'Events',
+      'add_new_item' => 'Add New Event',
+      'edit_item' => 'Edit Event',
+      'all_items' => 'All Events',
+      'singular_name' => 'Event'
+    ),
+    'menu_icon' => 'dashicons-calendar'
   ));
 }
 
